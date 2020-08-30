07326c8753680be06a5dea26e604fdcb47c362e9
MT2321 <mrodriguez@itba.edu.ar>
2020-08-30 14:21:42 -0300

    pulsador

--- Pul2Switch/source/board.h
+++ Pul2Switch/source/board.h
@@ -22,21 +22,17 @@
 
 // On Board User LEDs
 #define PIN_LED_RED     PORTNUM2PIN(PB,22)// PTB22
-
 #define PIN_LED_GREEN   PORTNUM2PIN(PE,26)// PTE26
 #define PIN_LED_BLUE    PORTNUM2PIN(PB,21) // PTB21
-#define PIN_PULSADOR 	PORTNUM2PIN(PC, 9) // PTC9
-#define PIN_LED_AMA 	PORTNUM2PIN(PB, 23)
-
 
 #define LED_ACTIVE      LOW
 
 
 // On Board User Switches
-#define PIN_SW2         PORTNUM2PIN(PC, 6)// PTC6???
-#define PIN_SW3         PORTNUM2PIN(PA,4)// PTA4
+#define PIN_SW2         // ???
+#define PIN_SW3         // ???
 
-#define SW_ACTIVE       LOW// ???
+#define SW_ACTIVE       // ???
 #define SW_INPUT_TYPE   // ???
 
 
