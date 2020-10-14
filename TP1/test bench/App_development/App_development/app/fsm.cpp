17aafc4e0ceaee941a1b9cc76c836ada77d91888
Guido <glambertucci@itba.edu.ar>
2020-10-14 12:22:08 -0300

    brightness state being developed

--- TP1/test bench/App_development/App_development/app/fsm.cpp
+++ TP1/test bench/App_development/App_development/app/fsm.cpp
@@ -2,6 +2,8 @@
  * INCLUDE HEADER FILES
  ******************************************************************************/
 #include "fsm.h"
+#include "../drivers/Timer.h"
+#include "../drivers/placaVerde.h"
  /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
@@ -10,8 +12,12 @@
   * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
   ******************************************************************************/
   /*******************************************************************************
-   * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
+   * VARIABLE PROTOTYPES WITH FILE SCOPE
    ******************************************************************************/
+static tim_id_t inactivity_timer_id;
+static encoder_id my_encoder_id;
+static bool inactivity_triggered;
+static state prev_state = IDDLE;
    /*******************************************************************************
 	* FUNCTION PROTOTYPES WITH FILE SCOPE
 	******************************************************************************/
@@ -52,15 +58,24 @@ state usersRoutine(void);
  * @return state variable with the updated state
  */
 state brightnessRoutine(void);
+///////////////////////////
+//     Callbacks        //
+//////////////////////////
+void inactivityCallback(void);
    /*******************************************************************************
 	* FUNCTION DEFINITION WITH GLOBAL SCOPE
 	******************************************************************************/
 
 state FSMInitState(void) {
-	return IDDLE;
+    inactivity_timer_id = timerGetId();
+    my_encoder_id = PVencoder_register();
+    timerStart(inactivity_timer_id, TIMER_MS2TICKS(INACTIVITY_TRIGGER_TIME), TIM_MODE_SINGLESHOT, inactivityCallback);
+    timerStop(inactivity_timer_id);
+    return IDDLE;
 }
 
 state FSMRun(state actual_state) {
+    
     state updated_state;
 	switch (actual_state) {
 	case IDDLE:
@@ -84,6 +99,7 @@ state FSMRun(state actual_state) {
 	default:
 			break;
 	}
+    prev_state = actual_state;
     return updated_state;
 }
 
@@ -110,7 +126,30 @@ state usersRoutine(void) {
     state updated_state = IDDLE;
     return updated_state;
 }
-state brightnessRoutine(void) {
-    state updated_state = IDDLE;
+state brightnessRoutine(void){
+    state updated_state = BRIGHTNESS;
+    //Si entré aca , el estado actual es brightness
+    //por lo que si el estado previo es distinto de brightenss 
+    //es la primera vez que entro, por lo que debo iniciar el timer de timout
+    if (prev_state != BRIGHTNESS) {
+        timerReset(inactivity_timer_id);
+        timerResume(inactivity_timer_id);
+    }
+    if (PVencoder_is_there_ev(my_encoder_id)) {
+        timerReset(inactivity_timer_id);
+        timerResume(inactivity_timer_id);
+        if(PVencoder_pop_event(my_encoder_id) == )
+    }
+
+    if (inactivity_triggered) {
+        updated_state = IDDLE;
+        inactivity_triggered = false;
+    }
     return updated_state;
+}
+/*******************************************************************************
+ * CALLBACKS DEFINITION
+ ******************************************************************************/
+void inactivityCallback(void) {
+    inactivity_triggered = true;
 }
