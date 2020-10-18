8441b22347b45e58e1a9261fff1a6df588132753
tlondero <tlondero@itba.edu.ar>
2020-10-18 16:06:48 -0300

    lo100to alan

--- TP1/TP1/TP1/SDK/startup/main.c
+++ TP1/TP1/TP1/SDK/startup/main.c
@@ -9,14 +9,19 @@
 void App_Init (void);
 void App_Run (void);
 
+void FRDM_tb_Init(void);
+void FRDM_tb_Run(void);
+
 
 int main (void)
 {
     hw_Init();
     hw_DisableInterrupts();
-    App_Init(); /* Program-specific setup */
+    //App_Init(); /* Program-specific setup */
+    FRDM_tb_Init();
     hw_EnableInterrupts();
 
     __FOREVER__
-        App_Run(); /* Program-specific loop  */
+        //App_Run(); /* Program-specific loop  */
+		FRDM_tb_Run();
 }
