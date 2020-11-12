f4735a31996c48b2c109a606dad294d170aed782
tlondero <tlondero@itba.edu.ar>
2020-11-12 18:41:58 -0300

    camino flexin por la street

--- Guia 5/I2CG3/Button/header/i2c.h
+++ Guia 5/I2CG3/Button/header/i2c.h
@@ -16,6 +16,10 @@
  ******************************************************************************/
 
 typedef enum {
+	I2C_NO_FAULT, I2C_BUS_BUSY, I2C_TIMEOUT, I2C_SLAVE_ERROR
+} I2C_FAULT;
+
+typedef enum {
 	I2C_0, I2C_1, I2C_2, I2C_COUNT
 } ic2_channel_t;
 
@@ -35,8 +39,9 @@ typedef void (*callbackptr)(void);
 
 bool i2cInit(ic2_channel_t chan);
 
-bool i2cTransaction(uint8_t slave_, uint8_t reg_, uint8_t* data_, uint8_t size_, i2c_mode_t mode_, callbackptr callback_);
+bool i2cTransaction(uint8_t slave_, uint8_t reg_, uint8_t *data_, uint8_t size_,
+		i2c_mode_t mode_, callbackptr callback_);
 
-void i2cIsrRoutine();
+I2C_FAULT i2cStatus(void);
 
 #endif // _I2C_H_
