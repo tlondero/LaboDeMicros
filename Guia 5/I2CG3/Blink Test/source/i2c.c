8d315618202b01acfa662787dd9e7ef2fdf3f2ef
Guido <glambertucci@itba.edu.ar>
2020-11-16 12:11:34 -0300

    ANDAAAAAAAAAAAAAA

--- Guia 5/I2CG3/Blink Test/source/i2c.c
+++ Guia 5/I2CG3/Blink Test/source/i2c.c
@@ -296,8 +296,8 @@ bool i2cTransaction(uint8_t slave_, uint8_t reg_, uint8_t *data_, uint8_t size_,
 
 		valid = true;
 	} else {
-		callback = NULL;
-		__asm("BKPT #0");
+//		callback = NULL;
+//		__asm("BKPT #0");
 		//i2cptr->C1 &= ~I2C_C1_MST_MASK;			//Send stop
 	}
 
