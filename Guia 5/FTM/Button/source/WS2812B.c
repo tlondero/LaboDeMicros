b9ee7afee40e6ed4d04d0ab2f424e4f4a758449a
Alan <31071359+amechu@users.noreply.github.com>
2020-11-21 15:44:36 -0300

    Update WS2812B.c

--- Guia 5/FTM/Button/source/WS2812B.c
+++ Guia 5/FTM/Button/source/WS2812B.c
@@ -12,10 +12,13 @@
 
 #define CNV_ON 39 //39 ticks -> 0.8us
 #define CNV_OFF 22 //22 ticks -> 0.46us
-#define CNV_ZERO 2
-#define MOD 63//62ticks ->1.26us
+#define CNV_ZERO 0
+#define MOD 62//62ticks ->1.26us
 
-#define MAT_SIZE (64+2)*8*3*2 //64 LEDS * 8BITS * 3 COLORES * 2bytes (CNV son 16 bits)
+#define CANT_LEDS 64
+#define CANT_LEDS_ZERO 10
+
+#define MAT_SIZE (CANT_LEDS+CANT_LEDS_ZERO)*8*3*2 //(64 LEDS+10LEDS en zero para reset) * 8BITS * 3 COLORES * 2bytes (CNV son 16 bits)
 #define ROW_SIZE 8
 
 typedef enum {RED, GREEN, BLUE} led_color;
@@ -26,9 +29,9 @@ typedef struct{
 	uint16_t B[8];
 }GRB_t;
 static int8_t ftmid1;
-static GRB_t led_matrix [64+2];//__attribute__((aligned(4096)));
+static GRB_t led_matrix [CANT_LEDS+CANT_LEDS_ZERO];
 
-static void set_led_brightness(uint16_t *ptr, uint8_t brightness){
+static void set_color_brightness(uint16_t *ptr, uint8_t brightness){
 	uint8_t i;
 	for (i = 0; i<8; i++){
 		ptr[i] = (brightness & (1<<i)) ? CNV_ON : CNV_OFF;
@@ -39,40 +42,23 @@ void WS2812B_init(void){
 	uint8_t i;
 	uint8_t j;
 
-	for(i = 0; i < 64; i++){
+	for(i = 0; i < CANT_LEDS+CANT_LEDS_ZERO; i++){
 
-		set_led_brightness(led_matrix[i].R, 255);
-		set_led_brightness(led_matrix[i].G, 255);
-		set_led_brightness(led_matrix[i].B, 255);
+		if(i < CANT_LEDS){
+		set_color_brightness(led_matrix[i].R, 0);
+		set_color_brightness(led_matrix[i].G, 0);
+		set_color_brightness(led_matrix[i].B, 0);
+		}
 
 	}
-	//Add pause leds
-	for(i = 0; i < 8; i++){
 
-		led_matrix[64].R[i] = CNV_ZERO;
-		led_matrix[64].G[i] = CNV_ZERO;
-		led_matrix[64].B[i] = CNV_ZERO;
-
-		led_matrix[65].R[i] = CNV_ZERO;
-		led_matrix[65].G[i] = CNV_ZERO;
-		led_matrix[65].B[i] = CNV_ZERO;
-	}
-
-
-
-
-/*
-	set_led_brightness(led_matrix[0].R, 255);
-	set_led_brightness(led_matrix[0].G, 255);
-	set_led_brightness(led_matrix[0].B, 255);
-*/
 	DMAInitWS2812b((uint16_t*)(&led_matrix), MAT_SIZE);
 
 	FTM_DATA data;
 	PORT_Init();
 	data.CNTIN = 0;
 	data.MODULO = MOD;	//62 ticks -> 1.26us
-	data.CNV = 0x005;
+	data.CNV = 0x00FF;
 	data.EPWM_LOGIC = FTM_lAssertedHigh;
 	data.MODE = FTM_mPulseWidthModulation;
 	data.PSC = FTM_PSC_x1;
