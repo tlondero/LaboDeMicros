e4311790d61bb5aa450985a853e37a3a8096ea4c
tlondero <tlondero@itba.edu.ar>
2020-11-28 13:21:32 -0300

    adc not dac

--- Guia 5/ADC/source/main.c
+++ Guia 5/ADC/source/main.c
@@ -13,35 +13,39 @@
 #include "ADC.h"
 #include "SysTick.h"
 
+#include "MK64F12.h"
 
 #define __FOREVER__ 	for(;;)
 
+void App_Run(void);
 
+int main(void) {
+	hw_Init();
+	PORT_Init();
+	GPIO_Init();
+	UART_Init();
+	SysTick_Init();
 
-int main (void)
-{
- 	 	 	 	hw_Init();
- 	 	 	 	PORT_Init();
- 	 	 		GPIO_Init();
- 	 	 		UART_Init();
- 	 	 		SysTick_Init();
+	ADC_Init(ADC0, ADC_b12, ADC_c4);
 
- 	 	 		ADC_Init();
+	UART_Send_Data('A');
+	UART_Send_Data('B');
+	UART_Send_Data('C');
 
+	// 		hw_DisableInterrupts();
 
-
- 	 	 			UART_Send_Data('A');
- 	 	 			UART_Send_Data('B');
- 	 	 			UART_Send_Data('C');
-
-
-
- 	 	// 		hw_DisableInterrupts();
-
- 	 	 		__FOREVER__;
-
-			// Enable interrupts
-			//hw_EnableInterrupts();
+	// Enable interrupts
+	hw_EnableInterrupts();
+	__FOREVER__
+		App_Run();
 
 }
 
+void App_Run(void) {
+	ADC_Start(ADC0, 0x00, ADC_mA);
+	uint8_t data;
+	//ADC0_DP0 SE
+	if (ADC_IsReady(ADC0)) {
+		data = ADC_getData(ADC0);
+	}
+}
